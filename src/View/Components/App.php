f3801af663405d6d56fc176ae7d6ddac71da8f14
Muath R Abu Ouda <muathabuouda@hotmail.com>
2026-09-16 01:18:20 +0300

    feat: initial release of ugarit/inertia v1.00.00

---
+++ src/View/Components/App.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Inertia\View\Components;
+
+use Heritage\View\Component;
+use Inertia\Ssr\Response;
+use Inertia\Ssr\SsrState;
+
+class App extends Component
+{
+    public ?Response $response;
+
+    public string $pageJson;
+
+    public function __construct(
+        public string $id = 'app',
+    ) {
+        $state = app(SsrState::class);
+        $this->response = $state->dispatch();
+        $this->pageJson = json_encode($state->page, JSON_HEX_TAG);
+    }
+
+    public function render(): string
+    {
+        return <<<'blade'
+@if($response)
+{!! $response->body !!}
+@else
+<script data-page="{{ $id }}" type="application/json">{!! $pageJson !!}</script><div id="{{ $id }}"></div>
+@endif
+blade;
+    }
+}
